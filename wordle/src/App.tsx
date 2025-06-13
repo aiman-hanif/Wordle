0fbd35059e68e56210a0864244908d0404f002bd
aiman-hanif <124278012+aiman-hanif@users.noreply.github.com>
2025-06-12 20:36:28 -0400

    add guess evaluation

--- wordle/src/App.tsx
+++ wordle/src/App.tsx
@@ -1,19 +1,32 @@
-import React, { useCallback, useState, useRef, KeyboardEvent, useEffect } from "react";
-
+import React, { useCallback, useState, useRef } from "react";
+import classNames from "classnames";
+enum GuessTypes {
+  CORRECT = "correct",
+  INCORRECT = "incorrect",
+  EXISTS = "exists",
+  NONE = "none"
+  ,
+}
 const NUM_GUESSES = 5;
 // next todo: figure out input one guess at a time
 function App() {
-  const name = "Aiman";
+  const name: string = "AIMAN";
   const max_entries = name.length * NUM_GUESSES;
   // read maintains the index for removing characters
   // write maintains the index for writing new characters
   const [focusedInput, setFocusedInput] = useState(0)
   const [currentGuess, setCurrentGuess] = useState(0)
   const [gameComplete, setGameComplete] = useState(false)
+  const [gameResult, setGameResult] = useState(false);
   const inputRef = useRef<(HTMLInputElement | null)[]>([]);
   const [inputGuesses, setInputGuesses] = useState(
     Array.from({ length: NUM_GUESSES }, () =>
       Array.from({ length: name.length }, () => "")
+    )
+  );
+  const [guessStatus, setGuessStatus] = useState(
+    Array.from({ length: NUM_GUESSES }, () =>
+      Array.from({ length: name.length }, () => GuessTypes.NONE)
     )
   );
 
@@ -24,11 +37,6 @@ function App() {
     return row * name.length + col;
   };
 
-  const generateRowCol = (index: number) => {
-    const row = Math.floor(index / NUM_GUESSES);
-    const col = index % NUM_GUESSES;
-    return { row, col };
-  };
   // valid inputs are 
   // letters
   // backspace
@@ -42,6 +50,27 @@ function App() {
       charCode === 69
     );
   };
+  const isGuessValid = () => {
+    return inputGuesses[currentGuess].join("").length === name.length
+  }
+
+  const evaluateGuess = () => {
+    const updatedGuessStatus = guessStatus
+    inputGuesses[currentGuess].map((letter, index) => {
+      if (letter === name.charAt(index)) {
+        updatedGuessStatus[currentGuess][index] = GuessTypes.CORRECT
+      }
+      else if (name.includes(letter)) {
+        updatedGuessStatus[currentGuess][index] = GuessTypes.EXISTS
+      }
+      else {
+        updatedGuessStatus[currentGuess][index] = GuessTypes.INCORRECT
+      }
+    })
+    setGuessStatus(updatedGuessStatus)
+    const gameWon = updatedGuessStatus[currentGuess].filter(item => item === GuessTypes.CORRECT).length === name.length
+    return gameWon
+  }
   const handleKeydown = useCallback(
     (code: string, key: string) => {
       // if game is complete then over
@@ -82,11 +111,24 @@ function App() {
 
       }
       else if (code === "Enter") {
+        if (!isGuessValid()) {
+          alert("Guess is not valid")
+          return
+        }
+        const gameResult = evaluateGuess();
+        setGameResult(gameResult)
+        const message = `Game won at ${currentGuess + 1} attempt${currentGuess > 0 ? 's' : ''}!`
+        if (gameResult) {
+          setGameComplete(true);
+          alert(message)
+          return;
+        }
+        console.log(guessStatus)
         newGuess += 1
         setCurrentGuess(newGuess);
         if (newGuess === NUM_GUESSES) {
           setGameComplete(true)
-          alert("Game is complete")
+          alert("Game is over...")
           return;
         }
         newFocusedInput = 0
@@ -99,7 +141,6 @@ function App() {
         updatedInput[currentGuess][focusedInput] = key.toUpperCase();
         setInputGuesses(updatedInput);
       }
-      // inputRef.current[generateIndex(newGuess, newFocusedInput.write)]?.focus();
     },
     [focusedInput, inputGuesses, max_entries]
   );
@@ -107,21 +148,27 @@ function App() {
   const renderInput = (row: number) => {
     return (
       <div className="flex gap-2">
-        {name.split("").map((_, index) => (
-          <input
-            onKeyDown={(e) => handleKeydown(e.code, e.key)}
-            ref={(item) => {
-              inputRef.current[generateIndex(row, index)] = item;
-            }}
-            readOnly={currentGuess !== row || focusedInput !== index}
-            maxLength={1}
-            type="text"
-            value={inputGuesses[row][index] ? inputGuesses[row][index] : ""}
-            className="caret-transparent text-md bg-default-background text-default-text border border-default-secondary text-center size-tile-md focus:outline-none"
-            key={index}
-            disabled={gameComplete}
-          ></input>
-        ))}
+        {name.split("").map((_, index) => {
+          const isCorrect = guessStatus[row][index] === GuessTypes.CORRECT
+          const isPresent = guessStatus[row][index] === GuessTypes.EXISTS
+          const isAbsent = guessStatus[row][index] === GuessTypes.INCORRECT
+          const isNone = guessStatus[row][index] === GuessTypes.NONE
+          return (
+            <input
+              onKeyDown={(e) => handleKeydown(e.code, e.key)}
+              ref={(item) => {
+                inputRef.current[generateIndex(row, index)] = item;
+              }}
+              readOnly={currentGuess !== row || focusedInput !== index}
+              maxLength={1}
+              type="text"
+              value={inputGuesses[row][index] ? inputGuesses[row][index] : ""}
+              className={classNames("caret-transparent text-md text-default-text border border-default-secondary text-center size-tile-md focus:outline-none transition-all", { "bg-correct": isCorrect, "bg-present": isPresent, "bg-absent": isAbsent, "bg-default-background": isNone })}
+              key={index}
+              disabled={gameComplete}
+            ></input>
+          )
+        })}
       </div>
     );
   };
